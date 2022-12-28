5ed9d3fbe99e4084fdb9771f0668bbb67df13b40
James White <jamesw6811@gmail.com>
2022-12-28 01:33:15 +0000

    allow sprites to be positioned inside the board

--- snowball-strategy-app/src/App.js
+++ snowball-strategy-app/src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import Board from './Board';
 
 function App() {
+  var sprites = [{x:5, y:12, image:0}, {x:15, y:3, image:0}, {x:0, y:0, image:0}];
   return (
-      <Board />
+      <Board sprites={sprites} />
   );
 }
 
--- snowball-strategy-app/src/Board.js
+++ snowball-strategy-app/src/Board.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Square from "./Square";
 import Sprite from "./Sprite";
+const SPRITESTYLE = {  position: "absolute", 
+    transform: "translate(-50%, -50%)", width: "5%", height: "5%"}
 
-export default function Board(){
+export default function Board({sprites, viewportWidth=100, unitsWidth=25.0, unitsHeight=25.0}){
     return (
-        <div style={{width:"100vw",height:"100vw", maxWidth:"100vh", maxHeight:"100vh"}}>
-        <Square><Sprite /></Square>
+        <div style={{position:"relative", width:`${viewportWidth}vw`,height:`${viewportWidth}vw`, 
+            maxWidth:"100vh", maxHeight:"100vh"}}>
+            {sprites.map((sprite, i) =>
+                <div key={i} style={{...SPRITESTYLE, 
+                    left: `${100.0*sprite.x/unitsWidth}%`, top: `${100.0*sprite.y/unitsHeight}%`}}>
+                    <Sprite image={sprite.image} />
+                </div>
+            )}
         </div>
     );
 }
